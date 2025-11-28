8ba9866801385933bc6537e899a5881c15da0375
jkemigisha <kjoseph1232018@gmail.com>
2025-11-28 19:26:06 +0300

    c

--- src/App.tsx
+++ src/App.tsx
@@ -15,7 +15,7 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter>
+      <BrowserRouter basename="/attendance_system/">
         <Routes>
           <Route path="/" element={<Index />} />
           <Route path="/auth" element={<Auth />} />
